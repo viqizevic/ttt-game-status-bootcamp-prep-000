1650bc6ba35bf65c3f3d89c3f46e4c8305f01ac2
viqi zevic <vicky.tanzil@gmail.com>
2018-09-13 20:49:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,6 +6,11 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
-  [3,4,5]  # Middle row
-  # ETC, an array for each win combination
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # First diagonal
+  [2,4,6]  # Second diagonal
 ]
