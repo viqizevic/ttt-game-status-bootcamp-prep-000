819e5f2c8c48eb0233944ab6d55644b2dd77183a
viqi zevic <vicky.tanzil@gmail.com>
2018-09-13 20:54:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,18 @@ WIN_COMBINATIONS = [
   [0,4,8], # First diagonal
   [2,4,6]  # Second diagonal
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+    if position_1 == position_2 && position_2 == position_3
+      return win_combination # return the win_combination indexes that won.
+    end
+  end
+  false
+end
